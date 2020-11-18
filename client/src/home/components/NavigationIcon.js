9b0f4c61405099e3bb6e4c10fc1f2c7cb5b845d0
Christina Gerard <ChristinaGerard@Christinas-MBP.hsd1.ga.comcast.net>
2020-11-17 22:02:43 -0500

    added hamburger with css styling to show when the user clicks on it

--- client/src/home/components/NavigationIcon.js
+++ client/src/home/components/NavigationIcon.js
@@ -3,88 +3,50 @@ import React, { useState } from "react";
 import "./NavigationIcon.css";
 
 const initialStyle = {
-  width: "2vw",
-  height: "2px",
+  width: "3vw",
+  height: "10px",
   backgroundColor: "#623b3c",
-  margin: "10px",
-  display: "block",
-  transition: "visibility 0s, opacity 0.5s linear"
+  borderRadius: "4px",
+  margin: "auto",
+  transition: ".09s",
+  transitionProperty: "height",
 };
 
-const firstBarStyle = {
+const barStyles = {
+  initial: initialStyle,
   active: {
-    transform: "rotate(45deg)",
-    left: "0",
-    right: "0",
-    top: "11px",
+    ...initialStyle,
     position: "absolute",
-    marginTop: "15px",
-    marginBottom: "15px",
-    transitionDuration: "0.5s",
-  },
-};
-
-const thirdBarStyle = {
-  active: {
-    transform: "rotate(-45deg)",
-    left: "0",
-    right: "0",
-    bottom: "11px",
-    position: "absolute",
-    marginTop: "15px",
-    marginBottom: "15px",
-    transitionDuration: "0.5s",
-  },
-};
-
-const secondBarStyle = {
-  active: {
-    visibility: "hidden",
+    height: "20px",
   },
 };
 
 const NavigationIcon = (props) => {
-  const [currentBarStyles, updateCurrentBarStyles] = useState({
-    one: initialStyle,
-    two: initialStyle,
-    three: initialStyle,
-  });
+  const [currentBarStyles, updateCurrentBarStyles] = useState(
+    barStyles.initial
+  );
+
   const [barStyleStatus, updateBarStyleStatus] = useState("initial");
 
   const handleStyleChange = () => {
     if (barStyleStatus === "initial") {
-      updateCurrentBarStyles({
-        ...currentBarStyles,
-        one: firstBarStyle.active,
-        two: secondBarStyle.active,
-        three: thirdBarStyle.active,
-      });
+      updateCurrentBarStyles(barStyles.active);
 
       updateBarStyleStatus("active");
-    } else {
-      updateCurrentBarStyles({
-        ...currentBarStyles,
-        one: initialStyle,
-        two: initialStyle,
-        three: initialStyle,
-      });
-
+    } else if (barStyleStatus === "active") {
+      updateCurrentBarStyles(barStyles.initial);
       updateBarStyleStatus("initial");
     }
   };
 
   return (
     <span className="navigationIcon__container" onClick={handleStyleChange}>
-      {["one", "two", "three"].map((order, idx) => {
+      {["one", "two"].map((order, idx) => {
         return (
           <div
             className={`navigationIcon__bar navigationIcon__bar-${order}`}
             key={idx}
-            style={
-              (order === "one" && currentBarStyles.one) ||
-              (order === "two" && currentBarStyles.two) ||
-              (order === "three" && currentBarStyles.three)
-            }
+            style={currentBarStyles}
           ></div>
         );
       })}
