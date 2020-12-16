1765b2e04dd02de2d62eda67addb7d944f9ca9be
Marlee Gerard <marleegerard@gmail.com>
2020-12-15 22:57:08 -0500

    adjusted auth widget

--- client/src/Auth/components/authentication-widget.js
+++ client/src/Auth/components/authentication-widget.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './authentication-widget.css'
 const AuthenticationWidget = props => {
 return <div className="AW-container center">
+    
     <h3>Sign in or Sign up</h3>
 </div>
 }
