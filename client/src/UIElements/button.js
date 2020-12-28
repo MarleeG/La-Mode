8d73b6ff7fabc774b38397bf7d51a8e3de50883f
Marlee Gerard <marleegerard@gmail.com>
2020-12-27 21:23:34 -0500

    added submit button to auth widet

--- client/src/UIElements/button.js
+++ client/src/UIElements/button.js
@@ -3,17 +3,20 @@ import React, { useEffect, useState } from "react";
 import "./button.css";
 
 const Button = (props) => {
-  const { text, styles, classes, hover } = props;
+  const { text, styles, classes, type, hover } = props;
   const [animatedClasses, updateAnimatedClasses] = useState("");
 
 //   useEffect(() => {
 //     if (hover) {
 //     }
 //   }, []);
+
+
   return (
     <button
       style={styles}
       className={`${classes} ${animatedClasses !== "" && animatedClasses}`}
+      type={type|| 'button'}
       onMouseEnter={() => {
         if (hover) {
           updateAnimatedClasses(hover);
