27abac5f10c317bb790caa7de9b7f2cbff9ffb9f
Marlee Gerard <marleegerard@gmail.com>
2020-12-27 21:42:28 -0500

    added input change functionality

--- client/src/UIElements/Input.js
+++ client/src/UIElements/Input.js
@@ -2,9 +2,15 @@ import React from "react";
 
 import "./Input.css";
 const Input = (props) => {
-  const { type, value, placeholder, handleInputChange } = props;
+  const { type, value, name, placeholder, handleInputChange } = props;
   return (
-    <input type={type || "text"} value={value} placeholder={placeholder} onChange={handleInputChange} />
+    <input
+      type={type || "text"}
+      value={value}
+      placeholder={placeholder}
+      onChange={handleInputChange}
+      name={name}
+    />
   );
 };
 
