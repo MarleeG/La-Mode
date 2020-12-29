a57fa7537b38e6600a1401570b1eaa4344410f72
Marlee Gerard <marleegerard@gmail.com>
2020-12-29 18:40:42 -0500

    added backdrop to homepage

--- client/src/home/components/SideDrawer.js
+++ client/src/home/components/SideDrawer.js
@@ -8,41 +8,24 @@ const SideDrawer = (props) => {
   const [currentAnimation, updateCurrentAnimation] = useState("growWidth");
 
   const [sideNavStyle, updateSideNavStyle] = useState({
-    // width: `${showNav ? "20%" : "0%"}`,
     width: "0%",
-
-    // display: `${showNav ? 'block': 'none'}`
   });
 
   useEffect(() => {
     if (showNav) {
-      // log(true);
-      // log(sideNavStyle);
-
-      // log(`showNav: ${showNav}`);
-
       updateSideNavStyle({
         ...sideNavStyle,
         width: "20%",
         animation: `growWidth 0.5s`,
       });
-      // log("---------");
     } else {
-      // log(false);
-      // log(sideNavStyle);
-      // log(`showNav: ${showNav}`);
-
-
-      if(openNavClickedOnce){
+      if (openNavClickedOnce) {
         updateSideNavStyle({
           ...sideNavStyle,
           width: "0%",
           animation: `shrinkWidth 0.5s`,
         });
       }
-      
-
-      // log("---------");
     }
 
     log(showNav);
