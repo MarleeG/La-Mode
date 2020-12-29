b42e1e8b1f3122b01026557389cddfc075d6ebb4
Marlee Gerard <marleegerard@gmail.com>
2020-12-29 18:01:01 -0500

    added back arrow to auth page

--- client/src/Auth/page/authentication.js
+++ client/src/Auth/page/authentication.js
@@ -1,16 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import AuthenticationWidget from "../components/authentication-widget";
 
 import "./authentication.css";
 
 const Auth = (props) => {
-  return <div className="auth_container center">
-    {/* center-text-in-div */}
-   <h2 className="auth__header font-ghotel">Á La Mode</h2>
+  return (
+    <div className="auth_container center">
+      {/* center-text-in-div */}
+      <Link to="/">
+        <div className="auth_container__back-arrow">⇐</div>
+      </Link>
 
+      <h2 className="auth__header font-ghotel">Á La Mode</h2>
 
-   <AuthenticationWidget/>
-  </div>;
+      <AuthenticationWidget />
+    </div>
+  );
 };
 
 export default Auth;
